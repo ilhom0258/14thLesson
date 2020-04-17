fb7d594db4866d31206c51fb2535b7f09b22cca2
unknown <ilhom0258@gmail.com>
2020-04-17 19:19:18 +0500

    Finish

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -6,6 +6,16 @@ namespace _14thLesson
 {
     public static class ArrayHelper
     {
+
+        
+        public static void show<T>(T[] array){
+            System.Console.Write("Current Elements: ");
+            foreach( var item in array ){
+                System.Console.Write(item + " ");
+            }
+        }
+
+
         /// <summary>
         /// Pop Method
         /// </summary>
@@ -34,17 +44,91 @@ namespace _14thLesson
             array = tempArray; 
             return last;  
         }
+
+        /// <summary>
+        /// Pushes an element to the end
+        /// </summary>
+        /// <param name="array">Array Type of T</param>
+        /// <param name="elementToPush">Element Type of T </param>
+        /// <typeparam name="T">Type of Object</typeparam>
+        /// <returns>size of new "array"</returns>
         public static int Push<T>(ref T[] array, T elementToPush ){
             
-            int lenghtOfNewArray; 
+            int lenghtOfNewArray = array.Length - 1; ; 
 
             var tempList = array.ToList();
             tempList.Add(elementToPush);
             array = tempList.ToArray();
-
-            lenghtOfNewArray = array.Length; 
             
             return lenghtOfNewArray; 
         }
+
+        /// <summary>
+        /// Takes out first element of "array"
+        /// </summary>
+        /// <param name="array">Array type of T</param>
+        /// <typeparam name="T">Type of Object</typeparam>
+        /// <returns>First element of "array"</returns>
+        public static T Shift<T>(ref T[] array){
+                
+                T firstElement; 
+
+                firstElement = array[0]; 
+                
+                T[] tempArray = new T[array.Length-1]; 
+
+                for ( int i = 0; i < array.Length - 1; i++ ){
+                    tempArray[i] = array[i+1]; 
+                }
+
+                array = tempArray; 
+                return firstElement; 
+       }
+
+        public static int UnShift<T>(ref T[] array, T elementToPush ){
+
+            int lenghtOfNewArray = array.Length + 1; 
+
+            var tempArray = array.ToList();
+            tempArray.Reverse();
+            tempArray.Add(elementToPush);
+            tempArray.Reverse(); 
+            array = tempArray.ToArray();
+
+            return lenghtOfNewArray; 
+        }
+        public static T[] Slice<T>(T[] array, int begin = 0, int end = 0){
+            
+            List<T> tempArray = new List<T>();
+            
+            int arraySize = array.Length;
+            if(end == 0){
+                end = array.Length;
+            }
+            if(begin > array.Length - 1){
+                return tempArray.ToArray();
+            }
+            else{
+                if(begin >= 0){
+                    if(end > 0){
+                        for(int i = begin; i < end; i++){
+                            tempArray.Add(array[i]);
+                        }
+                    }
+                    else if (end < 0){
+                        for(int i = begin; i < arraySize + end; i++){
+                            tempArray.Add(array[i]);
+                        }
+                    }
+                }
+                else if(begin < 0){
+                        for(int i = 0; i < (arraySize + begin); i++){
+                            tempArray.Add(array[i]);
+                        }
+                }
+                return tempArray.ToArray();
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Sql;
 using System.Data.SqlClient;
 
@@ -8,8 +9,66 @@ namespace _14thLesson
     {
         static void Main(string[] args)
         {
+            bool working = true;
+
+            string[] array = new string[]{"1", "2", "3"}; 
+
+            System.Console.WriteLine("Welcome to ArrayHelper:");
             
+            
+            while ( working ){
+
+                    System.Console.Write(@"
+                    Please choose function: 
+                    1. Pop(array)
+                    2. Push(array, element)
+                    3. Shift(array)
+                    4. UnShift(array, element)
+                    5. Slice(array, beginIndex, end Index)
+                    6. Exit
+                    Please choose reference number: ");
+                    int choice = int.Parse(Console.ReadLine());
+                    
+                    switch(choice){
+                        case 1:{
+                            System.Console.WriteLine("Popped element: "+ArrayHelper.Pop(ref array));
+                            ArrayHelper.show(array); 
+                        }break; 
+                        case 2:{
+                            string element = Console.ReadLine();
+                            System.Console.WriteLine("Size of Array: " + ArrayHelper.Push(ref array, element));
+                            ArrayHelper.show(array); 
+                        }break; 
+                        case 3:{
+                            System.Console.WriteLine("Shifted element: " + ArrayHelper.Shift(ref array));
+                            ArrayHelper.show(array); 
+                        }break; 
+                        case 4:{
+                            string element = Console.ReadLine(); 
+                            System.Console.WriteLine("Size of Array: " + ArrayHelper.UnShift(ref array, element));
+                            ArrayHelper.show(array); 
+                        }break;
+                        case 5:{   
+                            Console.Write("Begin Index: ");
+                            int begin = int.Parse(Console.ReadLine());
+
+                            Console.Write("End Index: ");
+                            int end = int.Parse(Console.ReadLine());
+
+                            string[] tempArray = ArrayHelper.Slice(array, begin, end);
+                            System.Console.Write("Sliced array: ");
+                            foreach( var item in tempArray ){
+                                System.Console.Write(item + " ");
+                            }
+
+                        }break; 
+                        case 6:{
+                            working = false; 
+                        }break; 
+                    } 
+            }
         }
+        
     }
 
 }
